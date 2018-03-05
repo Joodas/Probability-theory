925d29c10604a6e01b9a782a3c8312234dd131eb
bloodchainsaw <35767472+bloodchainsaw@users.noreply.github.com>
2018-03-05 18:01:55 +0300

    Add files via upload
    Собрал воедино свою часть лабы и часть лабы Женька. Мб стоит ещё над выводами поработать. Можешь сразу в этот файл код добавлять Никитос. Только вверху определи свою функцию

---
+++ Общий_концепт.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import math
+
+def Razmeshenie_s_povtorami(n, m):
+	return pow(n, m)
+
+def soch_bez_povtora(n, m): #отдельная функция для подсчёта сочетаний без повторений
+	c = math.factorial(n)/(math.factorial(m)*math.factorial(n-m)) #формула сочетаний без повторений
+	return c
+
+print ("Какую формулу вы хотите использовать? Введите 1, 2 или 3")
+print ("Formula = ", end = '')
+Formula = int(input(""))
+
+if (Formula == 1):
+	print ("Вы выбрали формулу: размещение с повторами")
+	print ("A = n^m")
+	print ("Введите значения n и m")
+	print ("n = ", end = '')
+	n = int(input(""))
+	print ("m = ", end = '')
+	m = int(input(""))
+	A = Razmeshenie_s_povtorami(n, m)
+	print ("A =", A)
+elif (Formula == 2):
+	print ("Вы выбрали формулу: число сочетаний без повтора")
+	print ("C[n, m] = n! / (m! * (n-m)!)")
+	print ("Введите значения n и m")
+	print ("n = ", end = '')
+	n = int(input(""))
+	print ("m = ", end = '')
+	m = int(input(""))
+
+	if (m > n):
+		print ("m должно быть меньше или равно n")
+		print ("Введите значения n и m")
+		print ("n = ", end = '')
+		n = int(input(""))
+		print ("m = ", end = '')
+		m = int(input(""))
+
+	print ("C =", int(soch_bez_povtora(n, m)))
+elif (Formula == 3): #Здесь должна быть часть кода Никитоса
+	print ("Вы выбрали формулу: число сочетаний с повторениями")
+	print ("С[n, m] = (n + m - 1)! / (m! * (n - 1)!)")
