9c339352c9f8496f1df6a2173ee0f4a961c37c27
Eugene <36998476+Joodas@users.noreply.github.com>
2018-03-06 22:01:09 +0300

    Update razmeshenie_s_povtorami.py

--- razmeshenie_s_povtorami.py
+++ razmeshenie_s_povtorami.py
@@ -1,15 +1,5 @@
 from __future__ import print_function
-import decimal
 import math
 
 def Razmeshenie_s_povtorami(n, m):
 	return pow(n, m)
-
-print ("Введите значения n и m")
-print ("n = ", end = '')
-n = int(input(""))
-print ("m = ", end = '')
-m = int(input(""))
-
-A = Razmeshenie_s_povtorami(n, m)
-print ("A =", A)
