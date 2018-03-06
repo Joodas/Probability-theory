2ee45e7380c63f5b18193ec50a6b2c6640dba06d
Eugene <36998476+Joodas@users.noreply.github.com>
2018-03-06 22:02:31 +0300

    Update sochetaniya_bez_povtora.py

--- sochetaniya_bez_povtora.py
+++ sochetaniya_bez_povtora.py
@@ -2,20 +2,3 @@ import math
 
 def combinations_without_repetition(n, m):                                  # отдельная функция для подсчёта сочетаний без повторений
     return (math.factorial(n)/(math.factorial(m)*math.factorial(n-m)))      # формула сочетаний без повторений
-
-print("Введите значения n и m")                                             #
-print("n = ", end = '')                                                     #
-n = int(input(""))                                                          # просим ввести значения
-print("m = ", end = '')                                                     #
-m = int(input(""))                                                          #
-
-if m > n:                                                                   # проверяем  m > n или нет
-                                                                            
-    print("m должно быть меньше или равно n ")                              
-    print("Введите значения n и m")                                         #
-    print("n = ", end = '')                                                 # повторныый ввод, если значчения не удволетворяют условию
-    n = int(input(""))                                                      #
-    print("m = ", end = '')                                                 #
-    m = int(input(""))                                                      #
-    
-print ("c = ", int(combinations_without_repetition(n, m)))                  # вывод результата
