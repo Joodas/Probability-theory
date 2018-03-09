fcff898b2e302cc483cc32721d6b479256b7c264
bloodchainsaw <35767472+bloodchainsaw@users.noreply.github.com>
2018-03-09 19:37:27 +0300

    Update 2nd_part.py
    Рабочий код второй части

--- 2nd_part.py
+++ 2nd_part.py
@@ -1,10 +1,9 @@
-from __future__ import print_function 
-from razmeshenie_s_povtorami import Razmeshenie_s_povtorami
-from sochetaniya_bez_povtora import combinations_without_repetition
+from __future__ import print_function
+from Razmeshenie import Razmeshenie_s_povtorami, Razmeshenie_bez_povtorov
 
-print ("Дана задача: \n В отделении связи поступило m телеграмм, которые случайным образом \n распределяются по каналам связи (n > m). Найти вероятность события A - на \n каждый канал придётся не больше одной телеграммы.")
+print ("Дана задача: \n В отделении связи поступило m телеграмм, которые случайным образом \n распределяются по n каналам связи(n > m). Найти вероятность события A - на \n каждый канал придётся не больше одной телеграммы.")
 
-print ("Для решения данной задачи воспользуемся следующей формулой: \n P(A) = C[1, n]*C[l, n - 1]*C[l, n - 2]*...*C[1, n - r + 1]*C[0, n - r]/A[n, m]")
+print ("Для решения данной задачи воспользуемся следующей формулой: \n P(A) = A[n, m]/A(повторы)[n, m]")
 
 print ("Введите значения n и m")
 print ("n = ", end = '')
@@ -12,17 +11,9 @@ n = int(input(""))
 print ("m = ", end = '')
 m = int(input(""))
 
-if (m > n):
-    print ("m должно быть меньше или равно n")
-    print ("Введите значения n и m")
-    print ("n = ", end = '')
-    n = int(input(""))
-    print ("m = ", end = '')
-    m = int(input(""))
-C_n = 1
-while (n >= 2):
-    C = int(combinations_without_repetition(n, m - 3))
-    C_n = C * C_n
-    n = n - 1
-P = C_n/Razmeshenie_s_povtorami(n, m)
-print ("P(A) = ", P)
+A1 = Razmeshenie_s_povtorami(n, m)
+A = Razmeshenie_bez_povtorov(n, m)
+
+P = A/A1
+
+print ("P(A) =", "%.20f" % P)
